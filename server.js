dd27ac90f1a1fbaf7fd77b6166eb45977afef2ac
Jinadu Ayotola O <ayo02034reel@yahoo.com>
2016-10-24 02:22:35 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+
+console.log("Hello, World!");
+
+var add = 1 + 3;
+console.log(add);
+
+var x = 0;
+do{
+x++;
+    console.log("x: " + x);
+}while (x<5)
